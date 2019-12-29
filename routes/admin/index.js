78484f229b471e861c4e111d19c120f9dbef102f
adibte <adibte@hotmail.com>
2019-12-30 01:58:47 +0330

    Faker added

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Admin/Post')
+const faker = require('faker')
 
 // Set layout
 router.all('/*',(req,res,next)=>{
@@ -9,6 +11,18 @@ router.all('/*',(req,res,next)=>{
 
 router.get('/',(req,res)=>{
     res.render('admin')
+})
+
+router.post('/gen_fake_post',(req,res)=>{
+    for(let i=0;i<req.body.amount;i++){
+        let post = new Post();
+        post.title = faker.lorem.sentences(2);
+        post.body = faker.lorem.paragraph(20);
+        post.allowComments = true;
+        post.status = "public";
+        post.save();
+    }
+    res.redirect('/admin/posts')
 })
 
 module.exports = router;
