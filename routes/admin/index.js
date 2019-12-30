f22d2a5743eed7c6dc50f98b805bc743fa3c9993
adibte <adibte@hotmail.com>
2019-12-31 02:58:49 +0330

    debug

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,42 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../../models/Admin/Post')
-const faker = require('faker')
+const Post = require('../../models/Admin/Post');
+const faker = require('faker');
 
 // Set layout
-router.all('/*',(req,res,next)=>{
+router.all('/*', (req, res, next) => {
     req.app.locals.layout = 'admin';
     next();
-})
+});
 
 // Index route
-router.get('/',(req,res)=>{
-    res.render('admin')
-})
+router.get('/', (req, res) => {
+    res.render('admin');
+});
 
 // Generates fake post
-router.post('/gen_fake_post',(req,res)=>{
-    Post.find().sort({ postId: -1 }).limit(1).then((sr)=>{
-        sr[0] != undefined ? global.postId = sr[0].postId : global.postId = 0;
-        for(let i=0;i<req.body.amount;i++){
+router.post('/gen_fake_post', (req, res) => {
+    Post.find().sort({ postId: -1 }).limit(1).then((sr) => {
+        sr[0] != undefined ? (global.postId = sr[0].postId) : (global.postId = 0);
+        for (let i = 0; i < req.body.amount; i++) {
             let post = new Post();
             post.title = faker.lorem.sentences(2);
             post.body = faker.lorem.paragraph(20);
             post.allowComments = true;
-            post.status = "public";
+            post.status = 'public';
             post.postId = ++global.postId;
             post.save();
         }
-        res.render('admin',{added:true})
-    })
-})
-
+        res.render('admin', { added: true });
+    });
+});
 
 // Delete all posts
-router.post('/removeAllPost',(req,res)=>{
-    Post.deleteMany({}).then(()=>{
-        res.render('admin',{deleted:true})
-    }).catch(err=>{console.log(err.message)})
-})
+router.post('/removeAllPost', (req, res) => {
+    Post.deleteMany({})
+        .then(() => {
+            res.render('admin', { deleted: true });
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
 
 module.exports = router;
