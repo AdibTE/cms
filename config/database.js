f6598427781561b8105d8e2641da348994e4c615
adibte <adibte@hotmail.com>
2020-02-07 03:33:37 +0330

    db change

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoDbUrl: 'mongodb://localhost:27017/CMS'
-        // mongoDbUrl: 'mongodb+srv://TEDB:Ai27RqEkY3Xv75rk@clusterfree-dft3g.azure.mongodb.net/CMS'
+    // mongoDbUrl: 'mongodb://localhost:27017/CMS'
+    mongoDbUrl: 'mongodb+srv://TEDB:Ai27RqEkY3Xv75rk@clusterfree-dft3g.azure.mongodb.net/CMS'
 };
